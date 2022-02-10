8d3f8f0fec6238d777c1976cdb2b15ff055452b0
Infilong <ytxqfe@gmail.com>
2022-02-10 20:34:16 +0800

    Edited CivicsApiService.kt

--- starter/app/src/main/java/com/example/android/politicalpreparedness/network/CivicsApiService.kt
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/network/CivicsApiService.kt
@@ -1,24 +1,37 @@
 package com.example.android.politicalpreparedness.network
 
+import com.example.android.politicalpreparedness.network.jsonadapter.ElectionAdapter
+import com.example.android.politicalpreparedness.network.models.ElectionResponse
+import com.example.android.politicalpreparedness.network.models.RepresentativeResponse
+import com.example.android.politicalpreparedness.network.models.VoterInfoResponse
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.squareup.moshi.Moshi
+import com.squareup.moshi.adapters.Rfc3339DateJsonAdapter
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import java.util.*
 
 private const val BASE_URL = "https://www.googleapis.com/civicinfo/v2/"
 
 // TODO: Add adapters for Java Date and custom adapter ElectionAdapter (included in project)
+//Moshi supports converting the primitive types, however the CivicsAPI also has Date type objects in some responses
+//and you will need to provide a converter for Moshi to understand this and convert the JSON date string
+// to a Date object instead of a normal String.
 private val moshi = Moshi.Builder()
-        .add(KotlinJsonAdapterFactory())
-        .build()
+    .add(Date::class.java, Rfc3339DateJsonAdapter())
+    .add(ElectionAdapter())
+    .add(KotlinJsonAdapterFactory())
+    .build()
 
 private val retrofit = Retrofit.Builder()
-        .addConverterFactory(MoshiConverterFactory.create(moshi))
-        .addCallAdapterFactory(CoroutineCallAdapterFactory())
-        .client(CivicsHttpClient.getClient())
-        .baseUrl(BASE_URL)
-        .build()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .addCallAdapterFactory(CoroutineCallAdapterFactory())
+    .client(CivicsHttpClient.getClient())
+    .baseUrl(BASE_URL)
+    .build()
 
 /**
  *  Documentation for the Google Civics API Service can be found at https://developers.google.com/civic-information/docs/v2
@@ -26,10 +39,16 @@ private val retrofit = Retrofit.Builder()
 
 interface CivicsApiService {
     //TODO: Add elections API Call
+    @GET("elections")
+    suspend fun getElections(): Call<List<ElectionResponse>>
 
     //TODO: Add voterinfo API Call
+    @GET("voterinfo")
+    suspend fun getVoterInfo(): Call<List<VoterInfoResponse>>
 
     //TODO: Add representatives API Call
+    @GET("representatives")
+    suspend fun getRepresentatives(): Call<List<RepresentativeResponse>>
 }
 
 object CivicsApi {
